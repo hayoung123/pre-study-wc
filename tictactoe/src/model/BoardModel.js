8e59748f4b29efb2991a70c1c5009d20253007ce
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-06-30 01:46:28 +0900

    fix. change filename grid to board

---
+++ tictactoe/src/model/BoardModel.js
@@ -0,0 +1,24 @@
+import Observable from '../util/observer.js';
+
+class BoardModel extends Observable {
+  constructor() {
+    super();
+    this.board = Array.from(new Array(3), (x) => new Array(3).fill(null));
+  }
+  getBoardState() {
+    return this.board;
+  }
+  setBoardState(x, y, turn) {
+    //home - 1 / away - 0
+    const mark = turn ? 1 : 0;
+    const board = deepCloneArray(this.board);
+    board[x][y] = mark;
+    this.board = board;
+    this.notify(this.board);
+  }
+  resetBoard() {
+    this.board = Array.from(new Array(3), (x) => new Array(3).fill(null));
+  }
+}
+
+export default BoardModel;
