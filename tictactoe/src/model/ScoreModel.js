355a144b33b9c22f828a7af6924eb40a8d5af6ff
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-06-30 01:02:31 +0900

    feat. make scoreModel

---
+++ tictactoe/src/model/ScoreModel.js
@@ -0,0 +1,20 @@
+import Observable from '../util/observer';
+
+class ScoreModel extends Observable {
+  constructor() {
+    super();
+    this.score = { home: 0, away: 0 };
+  }
+  getScore() {
+    return this.score;
+  }
+  setScore(turn) {
+    this.score = { ...this.score, [turn]: this.score[turn] + 1 };
+    this.notify(this.score);
+  }
+  resetScore() {
+    this.score = { home: 0, away: 0 };
+  }
+}
+
+export default ScoreModel;
