751f66ad3ab26a3c776bc7b9c6984392e4af890d
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-06-30 18:54:37 +0900

    fix. this 바인드 수정

--- tictactoe/src/components/score/Score.js
+++ tictactoe/src/components/score/Score.js
@@ -8,18 +8,18 @@ class Score {
     this.init();
   }
   init() {
-    this.scoreModel.subscribe(this.render);
-    this.turnModel.subscribe(this.render);
+    this.scoreModel.subscribe(this.render.bind(this));
+    this.turnModel.subscribe(this.render.bind(this));
     this.$target = createElement({ tagName: 'div', className: ['score__container'] });
     this.render();
   }
   render() {
     const score = this.scoreModel.getScore();
-    const turn = this.turnModel.getTurnState();
+    const turn = this.turnModel.getTurn();
     this.$target.innerHTML = this.getElement({ score, turn });
   }
   getElement({ score, turn }) {
-    const currentTurn = turn ? turn : this.turnModel.getTurnState();
+    const currentTurn = turn ? turn : this.turnModel.getTurn();
     return `<div class='score'>
                 <div>${score.home}</div>
                 <div class="score__colon">:</div>
