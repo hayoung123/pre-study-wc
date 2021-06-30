f4da80ce682ce552bf8bb15065e6c36a5019f8b5
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-06-30 21:47:07 +0900

    feat. 게임 승리 결과 구현

--- tictactoe/src/components/board/Board.js
+++ tictactoe/src/components/board/Board.js
@@ -4,21 +4,36 @@ import BoardSquare from './BoardSquare.js';
 class Board {
   constructor({ boardModel, turnModel, scoreModel }) {
     this.$target;
+    this.$resultTarget;
     this.boardModel = boardModel;
     this.turnModel = turnModel;
     this.scoreModel = scoreModel;
-    this.isFinish = false;
     this.init();
   }
   init() {
     this.boardModel.subscribe(this.render.bind(this));
     this.$target = createElement({ tagName: 'div', className: ['board__container'] });
+
+    this.$resultTarget = createElement({ tagName: 'div', className: ['game__result'] });
+    this.$target.appendChild(this.$resultTarget);
+
     this.$target.addEventListener('click', this.handleClick.bind(this));
     this.render();
   }
   render() {
     const board = this.boardModel.getBoard();
+    const turn = this.turnModel.getTurn() ? 'home' : 'away';
+    const score = this.scoreModel.getScore();
+
     this.$target.innerHTML = this.getElement(board);
+    this.$target.appendChild(this.$resultTarget);
+
+    if (this.isFinish(board)) {
+      this.$resultTarget.innerHTML = `${turn.toUpperCase()}님이 승리하셨습니다!!!`;
+      this.scoreModel.setScore({ ...score, [turn]: score[turn] + 1 });
+    } else {
+      this.$resultTarget.innerHTML = '';
+    }
   }
   getElement(board) {
     const boardElement = board.reduce((acc, boardRow, row) => {
@@ -42,10 +57,33 @@ class Board {
     const col = +dataset.col;
     const turn = this.turnModel.getTurn();
 
-    if (this.isFinish || board[row][col] !== null) return;
+    if (this.isFinish(board) || board[row][col] !== null) return;
 
     this.boardModel.setBoard(row, col, turn);
     this.turnModel.setTurnOver();
+  }
+  isFinish(board) {
+    const CHECK_LINE = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    const checkBoard = board.reduce((acc, row) => {
+      acc.push(...row);
+      return acc;
+    }, []);
+
+    for (const [a, b, c] of CHECK_LINE) {
+      if (checkBoard[a] && checkBoard[a] === checkBoard[b] && checkBoard[b] === checkBoard[c])
+        return true;
+    }
+
+    return false;
   }
 }
 
